95827eef21a11966e12dafd222249682aebbbb4b
melisand <melisandoval@live.com.ar>
2023-04-30 23:03:14 +0200

    add temporal programmatic navigation on Homepage

--- src/views/HomePage/HomePage.vue
+++ src/views/HomePage/HomePage.vue
@@ -8,12 +8,17 @@
 import { useDataStore } from "../../stores/dataStore";
 import { storeToRefs } from "pinia";
 import { onMounted } from "vue";
+import router from "../../router";
 
 const dataStore = useDataStore();
 const { listsData } = storeToRefs(dataStore);
 
 onMounted(async function () {
   await dataStore.getListsData();
+
+  // temporal programmatic navigation
+  router.push("/list-page/top-rated");
+
   console.log(listsData.value.topRated);
   console.log(listsData.value.popular);
   console.log(listsData.value.onTV);
